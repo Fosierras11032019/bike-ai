2af3d2c8586e0a28b5b3994d73c95fcadfb1e601
Fosierras11032019 <freddysierras@hotmail.com>
2026-01-02 15:29:02 -0500

    forzar paraque la interfaz local tenga los mismos colores que en cloud

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.set_page_config(
 
 #------------- Carga de estilos -------------
 def load_css():
-    with open("assets/styles.css") as f:
+    with open("assets/styles.css", encoding="utf-8") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
         
 load_css()
